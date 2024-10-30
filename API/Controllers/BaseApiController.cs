52cc72463340497319d8149031fe29e06672a32b
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2024-10-30 15:31:25 +0100

    Added pagination, product and productType models

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
         protected async Task<ActionResult> CreatedPagedResult<T>(IGenericRepository<T> repository, ISpecification<T> specification, int pageIndex, int pageSize) where T : BaseEntity
---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,11 @@
+export type Product = {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    currency: string;
+    prictureUrl: number;
+    productTypeId: string;
+    productBrandId: string;
+    city: string;
+}
