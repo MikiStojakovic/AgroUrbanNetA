8d9e7c8cf1c3d8c1415013f36a65fa4637ce15a2
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2024-10-31 15:37:57 +0100

    Added get product types and get product brands methods

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../../shared/models/product';
 import { Pagination } from '../../shared/pagination';
+import { ProductType } from '../../shared/models/productType';
+import { ProductBrand } from '../../shared/models/productBrand';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,24 @@ import { Pagination } from '../../shared/pagination';
 export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
   private http = inject(HttpClient);
+  types: ProductType[] = [];
+  brands: ProductBrand[] = [];
 
   getProducts() {
-    return this.http.get<Pagination<Product>>(this.baseUrl + 'products');
+    return this.http.get<Pagination<Product>>(this.baseUrl + 'products?pageSize=12');
+  }
+
+  getTypes() {
+    if (this.types.length > 0) return;
+    return this.http.get<ProductType[]>(this.baseUrl + "products/types").subscribe({
+      next: response => this.types = response
+    })
+  }
+  
+  getBrands() {
+    if (this.brands.length > 0) return;
+    return this.http.get<ProductBrand[]>(this.baseUrl + "products/brands").subscribe({
+      next: response => this.brands = response
+    })
   }
 }
