5f6b1232f3843b1fd8e5e20bdc722b1e0936b994
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2024-10-31 12:03:27 +0100

    Changed image paths

--- Infrastructure/Data/StoreDataSeed.cs
+++ Infrastructure/Data/StoreDataSeed.cs
@@ -58,7 +58,7 @@ namespace Infrastructure.Data
                         Description = "Zreli kravlji sir iz Sjenice",
                         Price = 750,
                         Currency = "RSD",
-                        PictureUrl = "images/products/zreliSir.png",
+                        PictureUrl = "/images/products/zreliSir.png",
                         ProductTypeId = Guid.Parse("e141e281-4c7d-4545-82be-662f3e0426d0"),
                         ProductBrandId = Guid.Parse("a0b5ea7d-36f8-4258-b598-bd73fd4e8da4"),
                         City = "Sjenica"
@@ -70,7 +70,7 @@ namespace Infrastructure.Data
                         Description = "Zreli kajmak od kravljeg mleka iz Sjenice",
                         Price = 750,
                         Currency = "RSD",
-                        PictureUrl = "images/products/domaciKajmak.png",
+                        PictureUrl = "/images/products/domaciKajmak.png",
                         ProductTypeId = Guid.Parse("e141e281-4c7d-4545-82be-662f3e0426d0"),
                         ProductBrandId = Guid.Parse("a0b5ea7d-36f8-4258-b598-bd73fd4e8da4"),
                         City = "Sjenica"
@@ -82,7 +82,7 @@ namespace Infrastructure.Data
                         Description = "Sveza neprskana sljiva sorte cacanska lepotica",
                         Price = 75,
                         Currency = "RSD",
-                        PictureUrl = "images/products/sljiva.png",
+                        PictureUrl = "/images/products/sljiva.png",
                         ProductTypeId = Guid.Parse("d7f12307-7930-4b48-bdb5-c2a66ddac524"),
                         ProductBrandId = Guid.Parse("62e811e5-955c-4839-bfb7-0cda054c20ba"),
                         City = "Valjevo"
@@ -94,7 +94,7 @@ namespace Infrastructure.Data
                         Description = "Sveza kruska viljamovka",
                         Price = 90,
                         Currency = "RSD",
-                        PictureUrl = "images/products/kruska.png",
+                        PictureUrl = "/images/products/kruska.png",
                         ProductTypeId = Guid.Parse("d7f12307-7930-4b48-bdb5-c2a66ddac524"),
                         ProductBrandId = Guid.Parse("62e811e5-955c-4839-bfb7-0cda054c20ba"),
                         City = "Valjevo"
@@ -106,7 +106,7 @@ namespace Infrastructure.Data
                         Description = "Domaci neprskani paradajz",
                         Price = 350,
                         Currency = "RSD",
-                        PictureUrl = "images/products/domaciParadajz.png",
+                        PictureUrl = "/images/products/domaciParadajz.png",
                         ProductTypeId = Guid.Parse("348d6a7a-98d1-45ac-9e7f-4cc3299e18c9"),
                         ProductBrandId = Guid.Parse("e1ba3f48-594b-4308-8ab8-d69faa322e2a"),
                         City = "Donji Petrovci"
@@ -118,7 +118,7 @@ namespace Infrastructure.Data
                         Description = "Crvena neprsana paprika sorte slonovo uvo",
                         Price = 120,
                         Currency = "RSD",
-                        PictureUrl = "images/products/crvenaPaprika.png",
+                        PictureUrl = "/images/products/crvenaPaprika.png",
                         ProductTypeId = Guid.Parse("348d6a7a-98d1-45ac-9e7f-4cc3299e18c9"),
                         ProductBrandId = Guid.Parse("e1ba3f48-594b-4308-8ab8-d69faa322e2a"),
                         City = "Donji Petrovci"
@@ -130,7 +130,7 @@ namespace Infrastructure.Data
                         Description = "Svinjski vrat sa koskom",
                         Price = 620,
                         Currency = "RSD",
-                        PictureUrl = "images/products/svinjskiVrat.png",
+                        PictureUrl = "/images/products/svinjskiVrat.png",
                         ProductTypeId = Guid.Parse("fa186b9a-9b5d-4b07-8df5-3dccef273f9a"),
                         ProductBrandId = Guid.Parse("653cc842-e8ba-4b1b-af05-72d7613b11f9"),
                         City = "Lezimir"
@@ -142,7 +142,7 @@ namespace Infrastructure.Data
                         Description = "Sveze pilece meso",
                         Price = 320,
                         Currency = "RSD",
-                        PictureUrl = "images/products/pileceMeso.png",
+                        PictureUrl = "/images/products/pileceMeso.png",
                         ProductTypeId = Guid.Parse("fa186b9a-9b5d-4b07-8df5-3dccef273f9a"),
                         ProductBrandId = Guid.Parse("653cc842-e8ba-4b1b-af05-72d7613b11f9"),
                         City = "Lezimir"
@@ -154,7 +154,7 @@ namespace Infrastructure.Data
                         Description = "Burek na stari nacin",
                         Price = 280,
                         Currency = "RSD",
-                        PictureUrl = "images/products/burek.png",
+                        PictureUrl = "/images/products/burek.png",
                         ProductTypeId = Guid.Parse("294ec6d5-aed0-45c9-8604-36d0bfa1d5de"),
                         ProductBrandId = Guid.Parse("b60a16c2-5756-44d0-81a6-b26f30ccfe8c"),
                         City = "Lezimir"
@@ -166,7 +166,7 @@ namespace Infrastructure.Data
                         Description = "Pita sa sjenickim sirom",
                         Price = 220,
                         Currency = "RSD",
-                        PictureUrl = "images/products/pitaSaSirom.png",
+                        PictureUrl = "/images/products/pitaSaSirom.png",
                         ProductTypeId = Guid.Parse("294ec6d5-aed0-45c9-8604-36d0bfa1d5de"),
                         ProductBrandId = Guid.Parse("b60a16c2-5756-44d0-81a6-b26f30ccfe8c"),
                         City = "Lezimir"
@@ -178,7 +178,7 @@ namespace Infrastructure.Data
                         Description = "Domaci med",
                         Price = 800,
                         Currency = "RSD",
-                        PictureUrl = "images/products/domaciMed.png",
+                        PictureUrl = "/images/products/domaciMed.png",
                         ProductTypeId = Guid.Parse("6c9901dc-ca5f-41e2-92d8-cb4ddc3a4578"),
                         ProductBrandId = Guid.Parse("88c46ef3-82f7-48e8-a055-a12bd3c32848"),
                         City = "Backi breg"
@@ -190,7 +190,7 @@ namespace Infrastructure.Data
                         Description = "Med u sacu",
                         Price = 1200,
                         Currency = "RSD",
-                        PictureUrl = "images/products/domaciMedUSacu.png",
+                        PictureUrl = "/images/products/domaciMedUSacu.png",
                         ProductTypeId = Guid.Parse("6c9901dc-ca5f-41e2-92d8-cb4ddc3a4578"),
                         ProductBrandId = Guid.Parse("88c46ef3-82f7-48e8-a055-a12bd3c32848"),
                         City = "Backi breg"
