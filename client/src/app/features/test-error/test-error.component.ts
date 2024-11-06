05bd14729d0b2899fe9ecf7ea023e08b5aa9841b
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2024-11-06 13:13:35 +0100

    Showing validation errors

--- client/src/app/features/test-error/test-error.component.ts
+++ client/src/app/features/test-error/test-error.component.ts
@@ -14,6 +14,7 @@ import { MatButton } from '@angular/material/button';
 export class TestErrorComponent {
   baseUrl = 'https://localhost:5001/api/';
   private http = inject(HttpClient);
+  validationErrors?: string[];
 
   get404Error() {
     this.http.get(this.baseUrl + 'buggy/notfound').subscribe({
@@ -42,7 +43,7 @@ export class TestErrorComponent {
   get400ValidationError() {
     this.http.post(this.baseUrl + 'buggy/validationerror', {}).subscribe({
       next: response => console.log(response),
-      error: error =>  console.log(error)
+      error: error =>  this.validationErrors = error
     })
   }
 }
