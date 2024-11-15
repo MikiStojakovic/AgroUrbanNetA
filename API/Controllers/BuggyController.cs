86ba3d6c3cb47f65f506e02e3e5bcb24c6eb4114
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2024-11-15 12:41:14 +0100

    Added GetSecret method

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,4 +1,6 @@
+using System.Security.Claims;
 using API.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RateLimiter.Attributes;
 
@@ -33,6 +35,15 @@ namespace API.Controllers
         public IActionResult GetValidationError(ProductDto product)
         {
             return Ok();
+        }
+        [Authorize]
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            var name = User.FindFirst(ClaimTypes.Name)?.Value;
+            var id = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            return Ok("Hello " + name + " with the id of " + id);
         }
     }
 }
