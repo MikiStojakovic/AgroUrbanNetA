9bb96d325407242d947a5dbea3d28c95c8d06db0
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2024-11-15 15:20:08 +0100

    Added GetUserByEmailWithAddress

--- API/Extensions/ClaimsPrincipleExtensions.cs
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -21,6 +21,15 @@ namespace API.Extensions
             return result;
         }
 
+        public static async Task<AppUser> GetUserByEmailWithAddress(this UserManager<AppUser> userManager, ClaimsPrincipal user)
+        {
+            var result = await userManager.Users.Include(x => x.Address).FirstOrDefaultAsync(x => x.Email == user.GetEmail());
+
+            if (result == null) throw new AuthenticationException("User not found");
+
+            return result;
+        }
+
         public static string GetEmail(this ClaimsPrincipal user)
         {
             var email = user.FindFirstValue(ClaimTypes.Email) ?? throw new AuthenticationException("Email not found");
