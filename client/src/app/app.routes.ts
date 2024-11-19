88d2cf28cf7decf1c1fe1f9fa047d79cdfec5098
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2024-11-19 10:30:39 +0100

    Fixed guard path in app routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -11,14 +11,13 @@ import { LoginComponent } from './features/account/login/login.component';
 import { RegisterComponent } from './features/account/register/register.component';
 import { authGuard } from './core/guards/auth.guard';
 import { checkCartItemsGuard } from './core/guards/check-cart-items.guard';
-import { emptyCartGuard } from './core/guards/empty-cart.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'shop', component: ShopComponent},
     {path: 'shop/:id', component: ProductDetailsComponent},
     {path: 'cart', component: CartComponent},
-    {path: 'checkout', component: CheckoutComponent, canActivate: [authGuard, emptyCartGuard]},
+    {path: 'checkout', component: CheckoutComponent, canActivate: [authGuard, checkCartItemsGuard]},
     {path: 'account/login', component: LoginComponent},
     {path: 'account/register', component: RegisterComponent},
     {path: 'test-error', component: TestErrorComponent},
